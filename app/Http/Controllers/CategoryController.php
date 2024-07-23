7ebe003eff4e2ac267d89178d92dd5e70967f944
prasetyodefan <prasetyodefan@gmail.com>
2024-07-23 10:55:52 +0700

    create relasi tabel

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+
+
+
 
 class CategoryController extends Controller
 {
@@ -30,40 +36,74 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $request->validate([
+            'name' => 'required|min:3',
+        ]);
+
+        // Store the category with the name field
+        Category::create([
+            'name' => $request->name,
+        ]);
+
+        return redirect()->route('admin.categories.index')->with('success', 'Category created successfully.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): View
     {
-        //
+        // Ambil kategori berdasarkan ID
+        $category = Category::findOrFail($id);
+
+        // Kembalikan tampilan dengan kategori
+        return view('admin.categories.show', compact('category'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id): View
     {
-        //
+        $category = Category::findOrFail($id);
+
+        return view('admin.categories.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): RedirectResponse
     {
-        //
+        // validate form
+        $request->validate([
+            'name' => 'required|min:5'
+        ]);
+
+        // find category
+        $category = Category::findOrFail($id);
+
+        // update category
+        $category->update($request->only('name'));
+
+        // redirect to index
+        return redirect()->route('admin.categories.index')->with('success', 'Data berhasil diupdate');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): RedirectResponse
     {
-        //
+        // find category
+        $category = Category::findOrFail($id);
+
+        // delete category
+        $category->delete();
+
+        // redirect to index
+        return redirect()->route('admin.categories.index')->with('success', 'Data berhasil dihapus');
     }
 }
